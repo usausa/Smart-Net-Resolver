f60c7464162fb55bca34be4741c6b8ac89834622
usausa <machi.pon@gmail.com>
2016-12-08 15:07:28 +0900

    Add assembly version

--- Smart.Resolver.AspNetCore/Properties/AssemblyInfo.cs
+++ Smart.Resolver.AspNetCore/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -10,6 +10,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Smart.Resolver.AspNetCore")]
 [assembly: AssemblyTrademark("")]
 
+[assembly: CLSCompliant(false)]
+
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
@@ -17,3 +19,6 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2d5e5003-862e-41ef-b85b-d8ded9c53a92")]
+
+[assembly: AssemblyVersion("1.1.2.0")]
+[assembly: AssemblyFileVersion("1.1.2.0")]
