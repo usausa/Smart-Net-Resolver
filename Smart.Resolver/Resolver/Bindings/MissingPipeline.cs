fa8aaf691b89a646927b82e792e0f036812ca553
usausa <machi.pon@gmail.com>
2016-12-14 13:34:15 +0900

    Update preparation of discards

--- Smart.Resolver/Resolver/Bindings/MissingPipeline.cs
+++ Smart.Resolver/Resolver/Bindings/MissingPipeline.cs
@@ -31,7 +31,7 @@
         /// <returns></returns>
         public IEnumerable<IBinding> Resolve(IResolverContext context, Type type)
         {
-            return Resolvers.SelectMany(b => b.Resolve(context, type));
+            return Resolvers.SelectMany(x => x.Resolve(context, type));
         }
     }
 }
