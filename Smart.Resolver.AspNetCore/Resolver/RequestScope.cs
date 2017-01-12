1050999aa68b201093fac5308574417b9cc5e379
usausa <machi.pon@gmail.com>
2017-01-12 19:23:50 +0900

    Disable RequestScope optimization

--- Smart.Resolver.AspNetCore/Resolver/RequestScope.cs
+++ Smart.Resolver.AspNetCore/Resolver/RequestScope.cs
@@ -1,23 +1,12 @@
 ﻿namespace Smart.Resolver
 {
-    using System.Threading;
-
     using Smart.Resolver.Scopes;
 
     public class RequestScope : IScope
     {
-        private volatile RequestScopeStorage storage;
-
         public IScopeStorage GetStorage(IKernel kernel)
         {
-#pragma warning disable 420
-            if (storage == null)
-            {
-                Interlocked.CompareExchange(ref storage, kernel.Get<RequestScopeStorage>(), null);
-            }
-#pragma warning restore 420
-
-            return storage;
+            return kernel.Get<RequestScopeStorage>();
         }
     }
 }
