c9d01dde3e30b31e794784e3a5b117993d238739
usausa <machi.pon@gmail.com>
2017-12-12 17:39:44 +0900

    Add FactoryResolver

--- Sandbox/WorkFactoryResolver/WorkFactoryResolver/Program.cs
+++ Sandbox/WorkFactoryResolver/WorkFactoryResolver/Program.cs
@@ -8,26 +8,6 @@
     {
         public static void Main(string[] args)
         {
-            //var resolver = new ObjectResolver();
-            //resolver.RegisterSingleton(typeof(Service1));
-            //resolver.RegisterSingleton(typeof(Service2));
-            //resolver.RegisterSingleton(typeof(Service3));
-            //resolver.RegisterTransient(typeof(SubObject1));
-            //resolver.RegisterTransient(typeof(SubObject2));
-            //resolver.RegisterTransient(typeof(SubObject3));
-            //resolver.RegisterTransient(typeof(Complex));
-
-            //var c1 = (Complex)resolver.Get(typeof(Complex));
-            //var c2 = (Complex)resolver.Get(typeof(Complex));
-            //System.Diagnostics.Debug.Assert(c1 != c2);
-            //System.Diagnostics.Debug.Assert(c1.Service1 == c2.Service1);
-            //System.Diagnostics.Debug.Assert(c1.Service2 == c2.Service2);
-            //System.Diagnostics.Debug.Assert(c1.Service3 == c2.Service3);
-            //System.Diagnostics.Debug.Assert(c1.SubObject1 != c2.SubObject1);
-            //System.Diagnostics.Debug.Assert(c1.SubObject2 != c2.SubObject2);
-            //System.Diagnostics.Debug.Assert(c1.SubObject3 != c2.SubObject3);
-
-
             BenchmarkSwitcher.FromAssembly(typeof(Program).GetTypeInfo().Assembly).Run(args);
         }
     }
--- Sandbox/WorkFactoryResolver/WorkFactoryResolver/Resolvers.cs
+++ Sandbox/WorkFactoryResolver/WorkFactoryResolver/Resolvers.cs
@@ -106,26 +106,188 @@
         object Create();
     }
 
+    public sealed class SingletonObjectFactory : IObjectFactory
+    {
+        private readonly object vaule;
+
+        public SingletonObjectFactory(object vaule)
+        {
+            this.vaule = vaule;
+        }
+
+        public object Create()
+        {
+            return vaule;
+        }
+    }
+
+    public sealed class Parameter0ObjectFactory : IObjectFactory
+    {
+        private readonly IActivator activator;
+
+        public Parameter0ObjectFactory(IActivator activator)
+        {
+            this.activator = activator;
+        }
+
+        public object Create()
+        {
+            return activator.Create();
+        }
+    }
+
+    public sealed class Parameter6ObjectFactory : IObjectFactory
+    {
+        private readonly IActivator activator;
+
+        private readonly IObjectFactory argumentFactory1;
+
+        private readonly IObjectFactory argumentFactory2;
+
+        private readonly IObjectFactory argumentFactory3;
+
+        private readonly IObjectFactory argumentFactory4;
+
+        private readonly IObjectFactory argumentFactory5;
+
+        private readonly IObjectFactory argumentFactory6;
+
+        public Parameter6ObjectFactory(
+            IActivator activator,
+            IObjectFactory argumentFactory1,
+            IObjectFactory argumentFactory2,
+            IObjectFactory argumentFactory3,
+            IObjectFactory argumentFactory4,
+            IObjectFactory argumentFactory5,
+            IObjectFactory argumentFactory6)
+        {
+            this.activator = activator;
+            this.argumentFactory1 = argumentFactory1;
+            this.argumentFactory2 = argumentFactory2;
+            this.argumentFactory3 = argumentFactory3;
+            this.argumentFactory4 = argumentFactory4;
+            this.argumentFactory5 = argumentFactory5;
+            this.argumentFactory6 = argumentFactory6;
+        }
+
+        public object Create()
+        {
+            return activator.Create(
+                argumentFactory1.Create(),
+                argumentFactory2.Create(),
+                argumentFactory3.Create(),
+                argumentFactory4.Create(),
+                argumentFactory5.Create(),
+                argumentFactory6.Create());
+        }
+    }
+
+    public static class FactoryProviderHelper
+    {
+        public static IObjectFactory Create(FactoryResolver resolver, IActivator activator)
+        {
+            var parameters = activator.Source.GetParameters();
+            var length = parameters.Length;
+
+            if (length == 0)
+            {
+                return new Parameter0ObjectFactory(activator);
+            }
+
+            if (length == 6)
+            {
+                return new Parameter6ObjectFactory(
+                    activator,
+                    resolver.GetFactory(parameters[0].ParameterType),
+                    resolver.GetFactory(parameters[1].ParameterType),
+                    resolver.GetFactory(parameters[2].ParameterType),
+                    resolver.GetFactory(parameters[3].ParameterType),
+                    resolver.GetFactory(parameters[4].ParameterType),
+                    resolver.GetFactory(parameters[5].ParameterType));
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+
     public interface IFactoryProvider
     {
-        IObjectFactory Resolve(IResolver resolver);
+        IObjectFactory Resolve(FactoryResolver resolver);
+    }
+
+    public sealed class SingletonFactoryProvider : IFactoryProvider
+    {
+        private readonly IActivator activator;
+
+        private IObjectFactory factory;
+
+        public SingletonFactoryProvider(Type type)
+        {
+            activator = TypeMetadataFactory.Default.CreateActivator(type.GetConstructors().First());
+        }
+
+        public IObjectFactory Resolve(FactoryResolver resolver)
+        {
+            if (factory == null)
+            {
+                var provider = FactoryProviderHelper.Create(resolver, activator);
+                factory = new SingletonObjectFactory(provider.Create());
+            }
+
+            return factory;
+        }
+    }
+
+    public sealed class TransientFactoryProvider : IFactoryProvider
+    {
+        private readonly IActivator activator;
+
+        public TransientFactoryProvider(Type type)
+        {
+            activator = TypeMetadataFactory.Default.CreateActivator(type.GetConstructors().First());
+        }
+
+        public IObjectFactory Resolve(FactoryResolver resolver)
+        {
+            return FactoryProviderHelper.Create(resolver, activator);
+        }
     }
 
     public sealed class FactoryResolver : IResolver
     {
+        private readonly Dictionary<Type, IFactoryProvider> providers = new Dictionary<Type, IFactoryProvider>();
+
+        private readonly Dictionary<Type, IObjectFactory> factories = new Dictionary<Type, IObjectFactory>();
+
         public void RegisterSingleton(Type type)
         {
-            throw new NotImplementedException();
+            providers[type] = new SingletonFactoryProvider(type);
         }
 
         public void RegisterTransient(Type type)
         {
-            throw new NotImplementedException();
+            providers[type] = new TransientFactoryProvider(type);
         }
 
         public object Get(Type type)
         {
-            throw new NotImplementedException();
+            return GetFactory(type)?.Create();
+        }
+
+        public IObjectFactory GetFactory(Type type)
+        {
+            if (!factories.TryGetValue(type, out var factory))
+            {
+                if (!providers.TryGetValue(type, out var provider))
+                {
+                    return null;
+                }
+
+                factory = provider.Resolve(this);
+                factories[type] = factory;
+            }
+
+            return factory;
         }
     }
 }
