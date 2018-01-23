0aab94d86de9f65db2d52a4249520eeaf963f7f3
usausa <machi.pon@gmail.com>
2018-01-23 13:20:02 +0900

    Update extensions

--- Smart.Resolver/Resolver/SmartResolverExtensions.cs
+++ Smart.Resolver/Resolver/SmartResolverExtensions.cs
@@ -26,6 +26,18 @@
             return resolver.CanGet(type, new NameConstraint(name));
         }
 
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", Justification = "Extensions")]
+        public static bool CanGet<T>(this IResolver resolver, string name)
+        {
+            return resolver.CanGet(typeof(T), new NameConstraint(name));
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", Justification = "Extensions")]
+        public static bool CanGet(this IResolver resolver, Type type, string name)
+        {
+            return resolver.CanGet(type, new NameConstraint(name));
+        }
+
         // Get
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", Justification = "Extensions")]
@@ -38,6 +50,18 @@
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", Justification = "Extensions")]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static object Get(this SmartResolver resolver, Type type, string name)
+        {
+            return resolver.Get(type, new NameConstraint(name));
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", Justification = "Extensions")]
+        public static T Get<T>(this IResolver resolver, string name)
+        {
+            return (T)resolver.Get(typeof(T), new NameConstraint(name));
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", Justification = "Extensions")]
+        public static object Get(this IResolver resolver, Type type, string name)
         {
             return resolver.Get(type, new NameConstraint(name));
         }
