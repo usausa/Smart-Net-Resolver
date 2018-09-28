8ab99d86e664771d8b808ef56be1285ffab220bb
usausa <machi.pon@gmail.com>
2018-09-28 23:51:15 +0900

    Add typed factory benchmark base

---
+++ Sandbox/TypedFactoryBenchmark/TypedFactoryBenchmark/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace TypedFactoryBenchmark
+{
+    using System;
+    using System.Reflection;
+
+    using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Configs;
+    using BenchmarkDotNet.Diagnosers;
+    using BenchmarkDotNet.Exporters;
+    using BenchmarkDotNet.Jobs;
+    using BenchmarkDotNet.Running;
+
+    using Smart.Collections.Concurrent;
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkSwitcher.FromAssembly(typeof(Program).GetTypeInfo().Assembly).Run(args);
+        }
+    }
+
+    public class BenchmarkConfig : ManualConfig
+    {
+        public BenchmarkConfig()
+        {
+            Add(MarkdownExporter.Default, MarkdownExporter.GitHub);
+            Add(MemoryDiagnoser.Default);
+            Add(Job.ShortRun);
+        }
+    }
+
+    public class FuncResolver
+    {
+        // TODO
+    }
+
+    public interface IFactory
+    {
+        object Create();
+    }
+
+    public interface IFactory<out T> : IFactory
+    {
+        T CreateTyped();
+    }
+
+    public class TypedFactoryResolver
+    {
+        // TODO
+    }
+
+    [Config(typeof(BenchmarkConfig))]
+    public class Benchmark
+    {
+        // TODO
+    }
+}
