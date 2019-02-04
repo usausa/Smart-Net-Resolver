e15648c4a611f8d7303fcd3a39d9a42e9f8a8875
usausa <machi.pon@gmail.com>
2019-02-04 18:53:49 +0900

    Remove old classes

--- Smart.Resolver.AspNetCore/Resolver/ResolverConfigExtensions.cs
+++ Smart.Resolver.AspNetCore/Resolver/ResolverConfigExtensions.cs
@@ -1,17 +1,17 @@
-﻿namespace Smart.Resolver
-{
-    using Microsoft.AspNetCore.Mvc.Controllers;
-    using Microsoft.AspNetCore.Mvc.ViewComponents;
+﻿//namespace Smart.Resolver
+//{
+//    using Microsoft.AspNetCore.Mvc.Controllers;
+//    using Microsoft.AspNetCore.Mvc.ViewComponents;
 
-    public static class ResolverConfigExtensions
-    {
-        public static ResolverConfig AddMvcActivatorSupport(this ResolverConfig config)
-        {
-            config.UseMissingHandler<ControllerMissingHandler>();
-            config.UseMissingHandler<ViewComponentMissingHandler>();
-            config.Bind<IControllerActivator>().To<SmartControllerActivator>().InSingletonScope();
-            config.Bind<IViewComponentActivator>().To<SmartViewComponentActivator>().InSingletonScope();
-            return config;
-        }
-    }
-}
+//    public static class ResolverConfigExtensions
+//    {
+//        public static ResolverConfig AddMvcActivatorSupport(this ResolverConfig config)
+//        {
+//            config.UseMissingHandler<ControllerMissingHandler>();
+//            config.UseMissingHandler<ViewComponentMissingHandler>();
+//            config.Bind<IControllerActivator>().To<SmartControllerActivator>().InSingletonScope();
+//            config.Bind<IViewComponentActivator>().To<SmartViewComponentActivator>().InSingletonScope();
+//            return config;
+//        }
+//    }
+//}
