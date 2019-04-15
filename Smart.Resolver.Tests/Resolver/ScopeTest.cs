cabaf8c9a1056ffe46444ea39958c9940825f741
usausa <machi.pon@gmail.com>
2019-04-15 09:54:31 +0900

    Update test

--- Smart.Resolver.Tests/Resolver/ScopeTest.cs
+++ Smart.Resolver.Tests/Resolver/ScopeTest.cs
@@ -44,6 +44,54 @@ namespace Smart.Resolver
         }
 
         [Fact]
+        public void ObjectInContainerScopeOfSameChildAreSame()
+        {
+            var config = new ResolverConfig();
+            config.Bind<SimpleObject>().ToSelf().InContainerScope();
+
+            using (var resolver = config.ToResolver())
+            using (var child = resolver.CreateChildResolver())
+            {
+                var obj1 = child.Get<SimpleObject>();
+                var obj2 = child.Get<SimpleObject>();
+
+                Assert.Same(obj1, obj2);
+            }
+        }
+
+        [Fact]
+        public void ObjectInContainerScopeOfDifferentChildAreDifferent()
+        {
+            var config = new ResolverConfig();
+            config.Bind<SimpleObject>().ToSelf().InContainerScope();
+
+            using (var resolver = config.ToResolver())
+            using (var child1 = resolver.CreateChildResolver())
+            using (var child2 = resolver.CreateChildResolver())
+            {
+                var obj1 = child1.Get<SimpleObject>();
+                var obj2 = child2.Get<SimpleObject>();
+
+                Assert.NotSame(obj1, obj2);
+            }
+        }
+
+        [Fact]
+        public void ObjectInContainerScopeOfParentAreDifferent()
+        {
+            var config = new ResolverConfig();
+            config.Bind<SimpleObject>().ToSelf().InContainerScope();
+
+            using (var resolver = config.ToResolver())
+            {
+                var obj1 = resolver.Get<SimpleObject>();
+                var obj2 = resolver.Get<SimpleObject>();
+
+                Assert.NotSame(obj1, obj2);
+            }
+        }
+
+        [Fact]
         public void ObjectInCustomScope()
         {
             var config = new ResolverConfig();
