3c1ea37351a5dfa38941165a04f105d5886000c9
usausa <machi.pon@gmail.com>
2019-04-17 14:08:12 +0900

    Update factory work

--- Sandbox/NewFactoryWork/NewFactoryLib/Builder.cs
+++ Sandbox/NewFactoryWork/NewFactoryLib/Builder.cs
@@ -1,7 +1,7 @@
 namespace NewFactoryLib
 {
     using System;
-    using System.Collections.Generic;
+    using System.Linq;
     using System.Reflection;
     using System.Reflection.Emit;
 
@@ -32,45 +32,41 @@ namespace NewFactoryLib
         }
 
 
-        public Func<IContainer, T> ToConstant<T>(T value)
+        public object ToConstant<T>(T value)
         {
-            return c => value;
+            return (Func<IContainer, T>)(c => value);
         }
 
-        public static Func<IContainer, T> ToMethod<T>(Func<IContainer, T> func)
+        public object ToMethod<T>(Func<IContainer, T> func)
         {
             return func;
         }
 
-        // TODO return to object ?
-        public Func<IContainer, T> To<T>(ConstructorInfo ci, params object[] factories)
+        public object To(ConstructorInfo ci, params object[] factories)
         {
             if (ci.GetParameters().Length != factories.Length)
             {
                 throw new ArgumentException("Invalid factories length.");
             }
 
-            // Type
+            // Define type
             var typeBuilder = ModuleBuilder.DefineType(
                 $"{ci.DeclaringType.FullName}_DynamicActivator{Array.IndexOf(ci.DeclaringType.GetConstructors(), ci)}",
                 TypeAttributes.Public | TypeAttributes.AutoLayout | TypeAttributes.AnsiClass | TypeAttributes.Sealed | TypeAttributes.BeforeFieldInit);
 
-            // Field
-            var fields = new List<FieldBuilder>();
-            for (var i = 0; i < factories.Length; i++)
-            {
-                fields.Add(typeBuilder.DefineField(
+            // Define field
+            var fields = factories
+                .Select((t, i) => typeBuilder.DefineField(
                     $"factory{i}",
-                    factories[i].GetType(),
-                    FieldAttributes.Public | FieldAttributes.InitOnly));
-            }
+                    t.GetType(),
+                    FieldAttributes.Public | FieldAttributes.InitOnly))
+                .ToList();
 
-            // TODO Final?
-            // Method
+            // Define method
             var method = typeBuilder.DefineMethod(
                 "Create",
                 MethodAttributes.Public | MethodAttributes.HideBySig,
-                typeof(T),
+                ci.DeclaringType,
                 new[] { typeof(IContainer) });
 
             var ilGenerator = method.GetILGenerator();
@@ -81,7 +77,6 @@ namespace NewFactoryLib
                 ilGenerator.Emit(OpCodes.Ldfld, fields[i]);
                 ilGenerator.Emit(OpCodes.Ldarg_1);
                 var invokeMethod = factories[i].GetType().GetMethod("Invoke", new[] { typeof(IContainer) });
-                // [MEMO] hack
                 ilGenerator.Emit(OpCodes.Call, invokeMethod);
             }
 
@@ -92,6 +87,7 @@ namespace NewFactoryLib
             var typeInfo = typeBuilder.CreateTypeInfo();
             var type = typeInfo.AsType();
 
+            // Prepare instance
             var instance = Activator.CreateInstance(type);
 
             for (var i = 0; i < factories.Length; i++)
@@ -100,12 +96,15 @@ namespace NewFactoryLib
                 fi.SetValue(instance, factories[i]);
             }
 
+            // Make delegate
+            var funcType = typeof(Func<,>).MakeGenericType(typeof(IContainer), ci.DeclaringType);
+
             // ReSharper disable once AssignNullToNotNullAttribute
-            return (Func<IContainer, T>)Delegate.CreateDelegate(typeof(Func<IContainer, T>), instance, type.GetMethod("Create"));
+            return Delegate.CreateDelegate(funcType, instance, type.GetMethod("Create"));
         }
 
-        // TODO Reflection version
-
         // TODO Array factory
+
+        // TODO Reflection version
     }
 }
--- Sandbox/NewFactoryWork/NewFactoryWork/Program.cs
+++ Sandbox/NewFactoryWork/NewFactoryWork/Program.cs
@@ -1,5 +1,7 @@
 namespace NewFactoryWork
 {
+    using System;
+
     using NewFactoryLib;
 
     class Program
@@ -8,17 +10,19 @@ namespace NewFactoryWork
         {
             var builder = new Builder();
 
-            var factorySingleton = builder.To<Singleton>(typeof(Singleton).GetConstructors()[0]);
-            //var singleton = factorySingleton(null);
+            var factorySingleton = builder.To(typeof(Singleton).GetConstructors()[0]);
+            var singleton = ((Func<IContainer, Singleton>)factorySingleton)(null);
+            var singleton2 = ((Func<IContainer, ISingleton>)factorySingleton)(null);
 
-            var factoryTransient = builder.To<Transient>(typeof(Transient).GetConstructors()[0]);
-            //var transient = factoryTransient(null);
+            var factoryTransient = builder.To(typeof(Transient).GetConstructors()[0]);
+            var transient = ((Func<IContainer, Transient>)factoryTransient)(null);
 
-            var factoryCombined = builder.To<Combined>(typeof(Combined).GetConstructors()[0], factorySingleton);
-            //var combined = factoryCombined(null);
+            var factoryCombined = builder.To(typeof(Combined).GetConstructors()[0], factorySingleton);
+            var combined = ((Func<IContainer, Combined>)factoryCombined)(null);
 
-            var factoryComplex = builder.To<Complex>(typeof(Complex).GetConstructors()[0], factoryTransient, factoryCombined);
-            var complex = factoryComplex(null);
+            var factoryComplex = builder.To(typeof(Complex).GetConstructors()[0], factoryTransient, factoryCombined);
+            var complex = ((Func<IContainer, Complex>)factoryComplex)(null);
+            var complex2 = ((Func<IContainer, object>)factoryComplex)(null);
         }
     }
 }
