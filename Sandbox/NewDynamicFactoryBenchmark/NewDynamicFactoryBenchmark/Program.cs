9fca4f52c6136fd8effd65c48fafe9508642967d
usausa <machi.pon@gmail.com>
2019-05-09 17:59:48 +0900

    Update benchmark

--- Sandbox/NewDynamicFactoryBenchmark/NewDynamicFactoryBenchmark/Program.cs
+++ Sandbox/NewDynamicFactoryBenchmark/NewDynamicFactoryBenchmark/Program.cs
@@ -1,9 +1,7 @@
 namespace NewDynamicFactoryBenchmark
 {
     using System;
-    using System.Linq;
     using System.Reflection;
-    using System.Reflection.Emit;
 
     using BenchmarkDotNet.Attributes;
     using BenchmarkDotNet.Configs;
@@ -11,6 +9,8 @@ namespace NewDynamicFactoryBenchmark
     using BenchmarkDotNet.Exporters;
     using BenchmarkDotNet.Jobs;
     using BenchmarkDotNet.Running;
+
+    using Smart.Resolver.Builders;
 
     public static class Program
     {
@@ -35,6 +35,9 @@ namespace NewDynamicFactoryBenchmark
     {
         private const int N = 1_000;
 
+        private static readonly Func<IResolver, object>[] EmptyFactories = Array.Empty<Func<IResolver, object>>();
+        private static readonly Action<IResolver, object>[] EmptyActions = Array.Empty<Action<IResolver, object>>();
+
         private static readonly ConstructorInfo ci0 = typeof(Data0).GetConstructors()[0];
         private static readonly ConstructorInfo ci1 = typeof(Data1).GetConstructors()[0];
         private static readonly ConstructorInfo ci2 = typeof(Data2).GetConstructors()[0];
@@ -43,109 +46,104 @@ namespace NewDynamicFactoryBenchmark
         private readonly Func<IResolver, object> stringFactory = r => string.Empty;
         private readonly Func<IResolver, object> intFactory = r => 0;
 
-        private Func<IResolver, object> dynamicFactory0;
-        private Func<IResolver, object> dynamicFactory1;
-        private Func<IResolver, object> dynamicFactory2;
-        private Func<IResolver, object> dynamicFactory4;
+        private Func<IResolver, object> oldDynamicFactory0;
+        private Func<IResolver, object> oldDynamicFactory1;
+        private Func<IResolver, object> oldDynamicFactory2;
+        private Func<IResolver, object> oldDynamicFactory4;
 
-        private Func<IResolver, object> classFactory0;
-        private Func<IResolver, object> classFactory1;
-        private Func<IResolver, object> classFactory2;
-        private Func<IResolver, object> classFactory4;
+        private Func<IResolver, object> newDynamicFactory0;
+        private Func<IResolver, object> newDynamicFactory1;
+        private Func<IResolver, object> newDynamicFactory2;
+        private Func<IResolver, object> newDynamicFactory4;
 
         [GlobalSetup]
         public void Setup()
         {
-            var builder = new Builder();
+            var oldBuilder = new OldEmitFactoryBuilder();
+            var newBuilder = new EmitFactoryBuilder();
 
-            dynamicFactory0 = builder.CreateDynamicFactory(ci0);
-            dynamicFactory1 = builder.CreateDynamicFactory(ci1, typeof(ParameterContainer1), new[] { stringFactory });
-            dynamicFactory2 = builder.CreateDynamicFactory(ci2, typeof(ParameterContainer2), new[] { stringFactory, intFactory });
-            dynamicFactory4 = builder.CreateDynamicFactory(ci4, typeof(ParameterContainer4), new[] { stringFactory, intFactory, stringFactory, intFactory });
+            oldDynamicFactory0 = oldBuilder.CreateFactory(ci0, EmptyFactories, EmptyActions);
+            oldDynamicFactory1 = oldBuilder.CreateFactory(ci1, new[] { stringFactory }, EmptyActions);
+            oldDynamicFactory2 = oldBuilder.CreateFactory(ci2, new[] { stringFactory, intFactory }, EmptyActions);
+            oldDynamicFactory4 = oldBuilder.CreateFactory(ci4, new[] { stringFactory, intFactory, stringFactory, intFactory }, EmptyActions);
 
-            classFactory0 = builder.CreateFactory(ci0, new Func<IResolver, object>[0]);
-            classFactory1 = builder.CreateFactory(ci1, new[] { stringFactory });
-            classFactory2 = builder.CreateFactory(ci2, new[] { stringFactory, intFactory });
-            classFactory4 = builder.CreateFactory(ci4, new[] { stringFactory, intFactory, stringFactory, intFactory });
+            newDynamicFactory0 = newBuilder.CreateFactory(ci0, EmptyFactories, EmptyActions);
+            newDynamicFactory1 = newBuilder.CreateFactory(ci1, new[] { stringFactory }, EmptyActions);
+            newDynamicFactory2 = newBuilder.CreateFactory(ci2, new[] { stringFactory, intFactory }, EmptyActions);
+            newDynamicFactory4 = newBuilder.CreateFactory(ci4, new[] { stringFactory, intFactory, stringFactory, intFactory }, EmptyActions);
         }
 
         [Benchmark(OperationsPerInvoke = N)]
-        public void DynamicFactory0()
+        public void OldDynamicFactory0()
         {
             for (var i = 0; i < N; i++)
             {
-                dynamicFactory0(null);
+                oldDynamicFactory0(null);
             }
         }
 
         [Benchmark(OperationsPerInvoke = N)]
-        public void ClassFactory0()
+        public void NewDynamicFactory0()
         {
             for (var i = 0; i < N; i++)
             {
-                classFactory0(null);
+                newDynamicFactory0(null);
             }
         }
 
         [Benchmark(OperationsPerInvoke = N)]
-        public void DynamicFactory1()
+        public void OldDynamicFactory1()
         {
             for (var i = 0; i < N; i++)
             {
-                dynamicFactory1(null);
+                oldDynamicFactory1(null);
             }
         }
 
         [Benchmark(OperationsPerInvoke = N)]
-        public void ClassFactory1()
+        public void NewDynamicFactory1()
         {
             for (var i = 0; i < N; i++)
             {
-                classFactory1(null);
+                newDynamicFactory1(null);
             }
         }
 
         [Benchmark(OperationsPerInvoke = N)]
-        public void DynamicFactory2()
+        public void OldDynamicFactory2()
         {
             for (var i = 0; i < N; i++)
             {
-                dynamicFactory2(null);
+                oldDynamicFactory2(null);
             }
         }
 
         [Benchmark(OperationsPerInvoke = N)]
-        public void ClassFactory2()
+        public void NewDynamicFactory2()
         {
             for (var i = 0; i < N; i++)
             {
-                classFactory2(null);
+                newDynamicFactory2(null);
             }
         }
 
         [Benchmark(OperationsPerInvoke = N)]
-        public void DynamicFactory4()
+        public void OldDynamicFactory4()
         {
             for (var i = 0; i < N; i++)
             {
-                dynamicFactory4(null);
+                oldDynamicFactory4(null);
             }
         }
 
         [Benchmark(OperationsPerInvoke = N)]
-        public void ClassFactory4()
+        public void NewDynamicFactory4()
         {
             for (var i = 0; i < N; i++)
             {
-                classFactory4(null);
+                newDynamicFactory4(null);
             }
         }
-    }
-
-    // Context
-
-    public interface IResolver
-    {
     }
 
     // Data
@@ -193,197 +191,6 @@ namespace NewDynamicFactoryBenchmark
             Parameter2 = parameter2;
             Parameter3 = parameter3;
             Parameter4 = parameter4;
-        }
-    }
-
-    // Container
-
-    public sealed class ParameterContainer1
-    {
-        public Func<IResolver, object> ParameterFactory0;
-    }
-
-    public sealed class ParameterContainer2
-    {
-        public Func<IResolver, object> ParameterFactory0;
-        public Func<IResolver, object> ParameterFactory1;
-    }
-
-    public sealed class ParameterContainer4
-    {
-        public Func<IResolver, object> ParameterFactory0;
-        public Func<IResolver, object> ParameterFactory1;
-        public Func<IResolver, object> ParameterFactory2;
-        public Func<IResolver, object> ParameterFactory3;
-    }
-
-    // Builder
-
-    public class Builder
-    {
-        private AssemblyBuilder assemblyBuilder;
-
-        private ModuleBuilder moduleBuilder;
-
-        private int nextId = 1;
-
-        private ModuleBuilder ModuleBuilder
-        {
-            get
-            {
-                if (moduleBuilder == null)
-                {
-                    assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(
-                        new AssemblyName("EmitBuilderAssembly"),
-                        AssemblyBuilderAccess.Run);
-                    moduleBuilder = assemblyBuilder.DefineDynamicModule(
-                        "EmitBuilderModule");
-                }
-
-                return moduleBuilder;
-            }
-        }
-
-        private int GenerateId()
-        {
-            lock (assemblyBuilder)
-            {
-                return nextId++;
-            }
-        }
-
-        // TODO dynamic create container type
-        public Func<IResolver, object> CreateDynamicFactory(ConstructorInfo ci, Type containerType, Func<IResolver, object>[] factories)
-        {
-            var returnType = ci.DeclaringType.IsValueType ? typeof(object) : ci.DeclaringType;
-
-            var dynamicMethod = new DynamicMethod(string.Empty, returnType, new[] { containerType, typeof(IResolver) }, true);
-            var ilGenerator = dynamicMethod.GetILGenerator();
-
-            var container = Activator.CreateInstance(containerType);
-
-            for (var i = 0; i < factories.Length; i++)
-            {
-                var invokeMethod = factories[i].GetType().GetMethod("Invoke");
-
-                var field = containerType.GetField($"ParameterFactory{i}");
-                field.SetValue(container, factories[i]);
-
-                ilGenerator.Emit(OpCodes.Ldarg_0);
-                ilGenerator.Emit(OpCodes.Ldfld, field);
-                ilGenerator.Emit(OpCodes.Ldarg_1);
-                ilGenerator.Emit(OpCodes.Call, invokeMethod);
-                if (ci.GetParameters()[i].ParameterType.IsValueType)
-                {
-                    ilGenerator.Emit(OpCodes.Unbox_Any, ci.GetParameters()[i].ParameterType);
-                }
-            }
-
-            ilGenerator.Emit(OpCodes.Newobj, ci);
-
-            if (ci.DeclaringType.IsValueType)
-            {
-                ilGenerator.Emit(OpCodes.Box, ci.DeclaringType);
-            }
-
-            ilGenerator.Emit(OpCodes.Ret);
-
-            var funcType = typeof(Func<,>).MakeGenericType(typeof(IResolver), returnType);
-            return (Func<IResolver, object>)dynamicMethod.CreateDelegate(funcType, container);
-        }
-
-        // TODO for combine
-        public Func<IResolver, object> CreateDynamicFactory(ConstructorInfo ci)
-        {
-            var returnType = ci.DeclaringType.IsValueType ? typeof(object) : ci.DeclaringType;
-
-            var dynamicMethod = new DynamicMethod(string.Empty, returnType, new[] { typeof(object), typeof(IResolver) }, true);
-            var ilGenerator = dynamicMethod.GetILGenerator();
-
-            ilGenerator.Emit(OpCodes.Newobj, ci);
-
-            if (ci.DeclaringType.IsValueType)
-            {
-                ilGenerator.Emit(OpCodes.Box, ci.DeclaringType);
-            }
-
-            ilGenerator.Emit(OpCodes.Ret);
-
-            var funcType = typeof(Func<,>).MakeGenericType(typeof(IResolver), returnType);
-            return (Func<IResolver, object>)dynamicMethod.CreateDelegate(funcType, null);
-        }
-
-        public Func<IResolver, object> CreateFactory(ConstructorInfo ci, Func<IResolver, object>[] factories)
-        {
-            var returnType = ci.DeclaringType.IsValueType ? typeof(object) : ci.DeclaringType;
-
-            // Define type
-            var typeBuilder = ModuleBuilder.DefineType(
-                $"{ci.DeclaringType.FullName}_Factory_{GenerateId()}",
-                TypeAttributes.Public | TypeAttributes.AutoLayout | TypeAttributes.AnsiClass | TypeAttributes.Sealed | TypeAttributes.BeforeFieldInit);
-
-            // Define field
-            var factoryFields = factories
-                .Select((t, i) => typeBuilder.DefineField(
-                    $"factory{i}",
-                    t.GetType(),
-                    FieldAttributes.Public | FieldAttributes.InitOnly))
-                .ToList();
-
-            // Define method
-            var method = typeBuilder.DefineMethod(
-                "Create",
-                MethodAttributes.Public | MethodAttributes.HideBySig,
-                returnType,
-                new[] { typeof(IResolver) });
-
-            var ilGenerator = method.GetILGenerator();
-
-            for (var i = 0; i < factories.Length; i++)
-            {
-                var invokeMethod = factories[i].GetType().GetMethod("Invoke");
-                if ((invokeMethod == null) ||
-                    (invokeMethod.GetParameters().Length != 1) ||
-                    (invokeMethod.GetParameters()[0].ParameterType != typeof(IResolver)))
-                {
-                    throw new ArgumentException($"Invalid factory[{i}]");
-                }
-
-                ilGenerator.Emit(OpCodes.Ldarg_0);
-                ilGenerator.Emit(OpCodes.Ldfld, factoryFields[i]);
-                ilGenerator.Emit(OpCodes.Ldarg_1);
-                ilGenerator.Emit(OpCodes.Call, invokeMethod);
-                if (ci.GetParameters()[i].ParameterType.IsValueType)
-                {
-                    ilGenerator.Emit(OpCodes.Unbox_Any, ci.GetParameters()[i].ParameterType);
-                }
-            }
-
-            ilGenerator.Emit(OpCodes.Newobj, ci);
-
-            if (ci.DeclaringType.IsValueType)
-            {
-                ilGenerator.Emit(OpCodes.Box, ci.DeclaringType);
-            }
-
-            ilGenerator.Emit(OpCodes.Ret);
-
-            var typeInfo = typeBuilder.CreateTypeInfo();
-            var factoryType = typeInfo.AsType();
-
-            // Prepare instance
-            var instance = Activator.CreateInstance(factoryType);
-
-            for (var i = 0; i < factories.Length; i++)
-            {
-                var fi = factoryType.GetField($"factory{i}");
-                fi.SetValue(instance, factories[i]);
-            }
-
-            // Make delegate
-            var funcType = typeof(Func<,>).MakeGenericType(typeof(IResolver), returnType);
-            // ReSharper disable once AssignNullToNotNullAttribute
-            return (Func<IResolver, object>)Delegate.CreateDelegate(funcType, instance, factoryType.GetMethod("Create"));
         }
     }
 }
--- Sandbox/NewDynamicFactoryBenchmark/README.md
+++ Sandbox/NewDynamicFactoryBenchmark/README.md
@@ -1,10 +1,10 @@
-|          Method |      Mean |     Error |    StdDev |  Gen 0 | Gen 1 | Gen 2 | Allocated |
-|---------------- |----------:|----------:|----------:|-------:|------:|------:|----------:|
-| DynamicFactory0 |  4.977 ns | 0.0153 ns | 0.0225 ns | 0.0057 |     - |     - |      24 B |
-|   ClassFactory0 |  5.884 ns | 0.0263 ns | 0.0393 ns | 0.0057 |     - |     - |      24 B |
-| DynamicFactory1 |  8.350 ns | 0.0209 ns | 0.0286 ns | 0.0057 |     - |     - |      24 B |
-|   ClassFactory1 |  8.317 ns | 0.0221 ns | 0.0331 ns | 0.0057 |     - |     - |      24 B |
-| DynamicFactory2 | 14.648 ns | 0.0484 ns | 0.0725 ns | 0.0133 |     - |     - |      56 B |
-|   ClassFactory2 | 14.817 ns | 0.0329 ns | 0.0451 ns | 0.0133 |     - |     - |      56 B |
-| DynamicFactory4 | 25.241 ns | 0.0593 ns | 0.0869 ns | 0.0210 |     - |     - |      88 B |
-|   ClassFactory4 | 25.750 ns | 0.0636 ns | 0.0932 ns | 0.0210 |     - |     - |      88 B |
+|             Method |      Mean |     Error |    StdDev |  Gen 0 | Gen 1 | Gen 2 | Allocated |
+|------------------- |----------:|----------:|----------:|-------:|------:|------:|----------:|
+| OldDynamicFactory0 |  5.329 ns | 0.0188 ns | 0.0270 ns | 0.0057 |     - |     - |      24 B |
+| NewDynamicFactory0 |  4.590 ns | 0.0273 ns | 0.0373 ns | 0.0057 |     - |     - |      24 B |
+| OldDynamicFactory1 |  8.029 ns | 0.0326 ns | 0.0488 ns | 0.0057 |     - |     - |      24 B |
+| NewDynamicFactory1 |  7.739 ns | 0.0185 ns | 0.0259 ns | 0.0057 |     - |     - |      24 B |
+| OldDynamicFactory2 | 13.712 ns | 0.0294 ns | 0.0440 ns | 0.0133 |     - |     - |      56 B |
+| NewDynamicFactory2 | 13.532 ns | 0.0304 ns | 0.0436 ns | 0.0133 |     - |     - |      56 B |
+| OldDynamicFactory4 | 24.277 ns | 0.2924 ns | 0.4194 ns | 0.0210 |     - |     - |      88 B |
+| NewDynamicFactory4 | 23.405 ns | 0.0545 ns | 0.0799 ns | 0.0210 |     - |     - |      88 B |
