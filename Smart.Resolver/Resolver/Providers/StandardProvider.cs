2e98ced24ea70f96572ac8076582b2e2464314ed
usausa <machi.pon@gmail.com>
2020-11-19 11:28:46 +0900

    Update provider

--- Smart.Resolver/Resolver/Providers/StandardProvider.cs
+++ Smart.Resolver/Resolver/Providers/StandardProvider.cs
@@ -62,6 +62,13 @@ namespace Smart.Resolver.Providers
                 {
                     var pi = parameter.Parameter;
 
+                    // Ignore self
+                    if (pi.ParameterType == TargetType)
+                    {
+                        match = false;
+                        break;
+                    }
+
                     // Constructor argument
                     var argument = binding.ConstructorArguments.GetParameter(pi.Name);
                     if (argument != null)
